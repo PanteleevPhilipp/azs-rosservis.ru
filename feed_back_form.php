8b0d1202a480cc51ee607355ff2a576a54242fe0
Panteleev Filipp <pheelpheelpheel@gmail.com>
2016-11-02 17:46:47 +0300

    release version

---
+++ feed_back_form.php
@@ -0,0 +1,34 @@
+﻿<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>ТЕСТ</title>
+</head>
+<body>
+
+<?php
+
+	$fio = $_POST['fio'];
+	$email = $_POST['email'];
+	$tel = $_POST['tel'];
+	$massage = $_POST['massage'];
+
+
+	$to = 'pheelpheelpheel@gmail.com';
+	$subject = 'Россервис. Фидбэк.';
+	$msg = "Имя отправителя: $fio\n" .
+		"Телефон отправителя: $tel\n".
+		"Email отправителя: $email\n" .
+		"Текст сообщения: $massage\n";
+	mail ($to, $subject, $msg, 'FROM: ' . $email);
+
+	echo "Спасибо за отправку формы. Возвращаемся назад... <br />";
+	echo "<script>history.go(-1)</script>";
+	
+
+
+
+
+?>
+
+</body>
+</html>
